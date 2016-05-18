74a86469e5c04a79305e8c408a8b067ca2cbc794
lixiaowei <lixiaowei@91yong.com>
2016-05-18 16:31:39 +0800

    添加用户反馈界面

--- app/src/main/java/com/ttm/tlrb/MainActivity.java
+++ app/src/main/java/com/ttm/tlrb/MainActivity.java
@@ -2,18 +2,19 @@ package com.ttm.tlrb;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity
+import com.ttm.tlrb.ui.activity.BaseActivity;
+import com.ttm.tlrb.ui.activity.FeedbackActivity;
+
+public class MainActivity extends BaseActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
@@ -27,8 +28,7 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                FeedbackActivity.launcher(MainActivity.this);
             }
         });
 
