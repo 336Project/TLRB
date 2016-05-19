eb61dc4232f21c6f800c70b79b842170d099cbb7
wei <1026181300@qq.com>
2016-05-19 17:58:11 +0800

    1.redbomb 类修改

--- app/src/main/java/com/ttm/tlrb/ui/entity/RedBomb.java
+++ app/src/main/java/com/ttm/tlrb/ui/entity/RedBomb.java
@@ -1,23 +1,25 @@
 package com.ttm.tlrb.ui.entity;
 
 import org.litepal.annotation.Column;
+import org.litepal.crud.DataSupport;
 
 /**
  * user:wtw
  * time: 2016/5/19 0019.
  */
-public class RedBomb {
-    private long id;
-    @Column(unique = true, defaultValue = "unknown",nullable = false)
-    private String userName;
-    private String name;
-    private String time;
-    private int target;
-    private int type;
-    private String categoryName;
-    private double money;
-    private String gift;
-    private String remark;
+public class RedBomb extends DataSupport {
+    private long id;//本地数据库id
+    @Column(unique = true, defaultValue = "",nullable = false)
+    private String userName;//数据关联的数据账号
+    private String name;//姓名
+    private String time;//时间
+    private int target;//男女方（1男方、2女方、3共同）
+    private int type;//类型（1收入、2支出）
+    private String categoryName;//组别名称（不设置，都为'默认'组别）
+    private double money;//金额
+    private String gift;//随礼
+    private String remark;//备注
+    private boolean isSync;//是否同步到服务器上面
 
     public long getId() {
         return id;
@@ -97,5 +99,13 @@ public class RedBomb {
 
     public void setRemark(String remark) {
         this.remark = remark;
+    }
+
+    public boolean isSync() {
+        return isSync;
+    }
+
+    public void setIsSync(boolean isSync) {
+        this.isSync = isSync;
     }
 }
