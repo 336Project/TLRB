4bd644405f28a02a945af1fdb8df629ded452761
zhouxiaokang <zhouxiaokang@qq.com>
2016-06-27 17:06:34 +0800

    晓康 提交

---
+++ app/src/main/java/com/ttm/tlrb/ui/activity/UpdateNickNameActivity.java
@@ -0,0 +1,15 @@
+package com.ttm.tlrb.ui.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.ttm.tlrb.R;
+
+public class UpdateNickNameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_nick_name);
+    }
+}
