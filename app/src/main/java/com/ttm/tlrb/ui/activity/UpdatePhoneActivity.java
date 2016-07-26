347c422e5e7f686039574143abc66806e5150f45
lixiaowei <lixiaowei@91yong.com>
2016-07-26 11:04:23 +0800

    绑定手机修改

--- app/src/main/java/com/ttm/tlrb/ui/activity/UpdatePhoneActivity.java
+++ app/src/main/java/com/ttm/tlrb/ui/activity/UpdatePhoneActivity.java
@@ -11,13 +11,23 @@ import com.ttm.tlrb.R;
 import com.ttm.tlrb.api.APIManager;
 import com.ttm.tlrb.api.UserManager;
 import com.ttm.tlrb.api.e.HttpExceptionHandle;
+import com.ttm.tlrb.ui.application.Constant;
 import com.ttm.tlrb.ui.entity.Account;
 import com.ttm.tlrb.ui.entity.BmobObject;
+import com.ttm.tlrb.utils.GsonUtil;
 import com.ttm.tlrb.utils.ToastUtil;
 import com.ttm.tlrb.utils.VerifyUtil;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import okhttp3.RequestBody;
 import retrofit2.adapter.rxjava.HttpException;
+import rx.Observable;
 import rx.Subscriber;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Func1;
+import rx.schedulers.Schedulers;
 
 public class UpdatePhoneActivity extends TitlebarActivity implements View.OnClickListener{
 
@@ -65,7 +75,7 @@ public class UpdatePhoneActivity extends TitlebarActivity implements View.OnClic
     @Override
     public void onClick(View v) {
         switch (v.getId()){
-            case R.id.btn_confirm:
+            case R.id.btn_confirm://绑定手机
                 final String phone = mEditTextPhone.getText().toString().trim();
                 String code = mEditTextCode.getText().toString().trim();
                 if(!VerifyUtil.checkMobileNumber(phone)){
@@ -76,35 +86,23 @@ public class UpdatePhoneActivity extends TitlebarActivity implements View.OnClic
                     ToastUtil.showToast(UpdatePhoneActivity.this,"请输入验证码");
                     return;
                 }
-                APIManager.getInstance().verifySmsCode(phone, code, new Subscriber<BmobObject>() {
-                    @Override
-                    public void onCompleted() {
-
-                    }
-
-                    @Override
-                    public void onError(Throwable e) {
-                        if(e instanceof HttpException){
-                            HttpExceptionHandle handle = new HttpExceptionHandle((HttpException) e,UpdatePhoneActivity.this);
-                            handle.handle();
-                        }else {
-                            ToastUtil.showToast(UpdatePhoneActivity.this,"验证失败");
-                        }
-                    }
-
-                    @Override
-                    public void onNext(BmobObject object) {
-                        //验证成功，更新用户信息
-                        Account account = UserManager.getInstance().getAccount();
-                        account.setMobilePhoneNumberVerified(true);
-                        account.setMobilePhoneNumber(phone);
-                        UserManager.getInstance().updateAccount(account);
-
-                        Account updateAccount = new Account();
-                        updateAccount.setObjectId(account.getObjectId());
-                        updateAccount.setMobilePhoneNumber(phone);
-                        updateAccount.setMobilePhoneNumberVerified(true);
-                        APIManager.getInstance().updateUser(updateAccount, new Subscriber<BmobObject>() {
+                Map<String,String> map = new HashMap<>();
+                map.put("mobilePhoneNumber",phone);
+                RequestBody body = RequestBody.create(Constant.JSON, GsonUtil.fromMap2Json(map));
+                APIManager.getInstance().getAPIService().verifySmsCode(code,body)
+                        .flatMap(new Func1<BmobObject, Observable<BmobObject>>() {
+                            @Override
+                            public Observable<BmobObject> call(BmobObject object) {
+                                Account updateAccount = new Account();
+                                updateAccount.setMobilePhoneNumber(phone);
+                                updateAccount.setMobilePhoneNumberVerified(true);
+                                RequestBody body = RequestBody.create(Constant.JSON, updateAccount.toString());
+                                return APIManager.getInstance().getAPIService().putUser(mAccount.getObjectId(),body);
+                            }
+                        })
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe(new Subscriber<BmobObject>() {
                             @Override
                             public void onCompleted() {
 
@@ -122,15 +120,17 @@ public class UpdatePhoneActivity extends TitlebarActivity implements View.OnClic
 
                             @Override
                             public void onNext(BmobObject object) {
+                                Account account = UserManager.getInstance().getAccount();
+                                account.setMobilePhoneNumberVerified(true);
+                                account.setMobilePhoneNumber(phone);
+                                UserManager.getInstance().updateAccount(account);
                                 ToastUtil.showToast(UpdatePhoneActivity.this,"绑定成功");
                                 setResult(RESULT_OK);
                                 finish();
                             }
                         });
-                    }
-                });
                 break;
-            case R.id.btn_get_code:
+            case R.id.btn_get_code://获取验证码
                 String phoneNum = mEditTextPhone.getText().toString().trim();
                 if(VerifyUtil.checkMobileNumber(phoneNum)){
                     mCountDownTimer.start();
